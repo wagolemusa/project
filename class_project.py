8a4ea73d8a608b24257f417469aed3e03bd73f44
kisumutalent <homiemusa@gmail.com>
2018-06-27 13:52:41 +0300

    complite system

--- class_project.py
+++ class_project.py
@@ -6,6 +6,7 @@ def user():
     print "2) Login"
     print "3) Post content"
     print "4) comment"
+    print "5) logout"
     print " "
 
 def register(full_name,username,email,password,confirm_password):
@@ -19,3 +20,21 @@ def post(title,content):
 
 def comment(comment):
     print("post your comments here!")
+
+loop = 1
+choice = 0
+while loop == 1:
+    choice == user()
+    if choice == 1:
+        register(input("full_name"),input("username"),input("email"),input("passwors"),input("confrim_password"))
+    elif choice == 2:
+        login(input("email"),input("password"))
+    elif choice == 3:
+        post(input("title"), input("content"))
+    elif choice == 4:
+        comment(input(comment))
+
+    elif choice == 5:
+        loop == 0
+    print "Thanks you for using our System"
+    
