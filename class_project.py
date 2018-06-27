12c8d1c0269ea5bd6d65d3dc0cf3e031b36a63dc
kisumutalent <homiemusa@gmail.com>
2018-06-27 13:24:58 +0300

    declaring metheds

--- class_project.py
+++ class_project.py
@@ -1,6 +1,4 @@
-loop = 1
-choice = 0
-while loop == 1:
+def user():
     print"We come to our System"
     print"What do you want to do?"
     print " "
@@ -9,4 +7,15 @@ while loop == 1:
     print "3) Post content"
     print "4) comment"
     print " "
-    
+
+def register(full_name,username,email,password,confirm_password):
+    print "full in the field to register"
+
+def login(email,password):
+    print"Login with your correct  username and password"
+
+def post(title,content):
+    print "post your content here!"
+
+def comment(comment):
+    print("post your comments here!")
