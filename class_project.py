e13e982bde7ea5e2b62efbce63b343a6cea6f8d2
kisumutalent <homiemusa@gmail.com>
2018-06-27 13:06:16 +0300

    setting up the options

---
+++ class_project.py
@@ -0,0 +1,12 @@
+loop = 1
+choice = 0
+while loop == 1:
+    print"We come to our System"
+    print"What do you want to do?"
+    print " "
+    print "1) Registration"
+    print "2) Login"
+    print "3) Post content"
+    print "4) comment"
+    print " "
+    
