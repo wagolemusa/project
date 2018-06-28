e65485c6bb805926d92ab7be1a7fd3d9d380be7a
kisumutalent <homiemusa@gmail.com>
2018-06-28 12:02:28 +0300

    updating the systm

--- system.py
+++ system.py
@@ -1,45 +1,58 @@
 class User():
-
+    def __init__(self):
+        self.databese={}
     #enables users to create accounts
     def register(self):
-        print"*****************************"
-        print"Welcome To Our System"
-        print " "
-        full_name = raw_input("Enter Your Names")
-        username  = raw_input("Enter Your Username")
-        email     = raw_input("Enter Your Email")
-        password  = raw_input("Enter Your password")
-        confirm_password = raw_input("Confirm Your passward")
+        print('*****************************')
+        print('Welcome To Our System')
+        print (' ')
+        full_name = input('Enter Your Names ')
+        username  = input('Enter Your Username ')
+        email     = input('Enter Your Email  ')
+        password  = input('Enter Your password ')
+        confirm_password = input('Confirm Your passward ')
+        self.reg(full_name,username,email,password,confirm_password)
 
-        print"You are successfully Signup"
-        print " " 
+    def reg(self,full_name,username,email,password,confirm_password):
+        self.databese.update({username:password})
+        print('Successfully Signup' + " " + username)
 
-        print"Would do you like to sigin?"
-        print " "
-        print"*********************************"
+        print (' ')
+
+        print('Would do you like to sigin?')
+        print ('')
+        print('*********************************')
         
         #enable users to Signin
     def login(self):
-        username = raw_input("Enter Your Username")
-        password = raw_input("Enter Your Password")
-        print " "
-        print"Welcome %s You successfully logged in" %username
-        print " "
-        print"***********************************"
-        print"You go a head and post Your content"
+        username = input('Enter Your Username')
+        password = input('Enter Your Password')
+        self.log(username,password)
+
+    def log(self,username, passward):
+        if passward==self.databese[username]:
+            print (' ')
+            print('Welcome  You successfully logged in' + " " +username)
+        else:
+            print('Invalid Credentials')
+            
+        print (' ')
+        print('***********************************')
+        print('You go a head and post Your content')
+
         #enable user to post content
     def post(self):
-        title = raw_input("Enter Title")
-        content = raw_input("Enter Content Here")
-        print" "
+        title = input('Enter Title')
+        content = input('Enter Content Here')
+        print('')
 
-        print"Would do like to comment on your post"
-        print"**************************************"
+        print('Would do like to comment on your post')
+        print('**************************************')
         
         #enables other people to post there reviews
     def post_comment(self):
-        comment = raw_input("Post Your reviews here!")
-        print"***************************************"
+        comment = input('Post Your reviews here!')
+        print('***************************************')
 
 system = User()
 system.register()
