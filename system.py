6e6e6e9bd8948cf6b5fd7e1cd90f8bf7abe924c9
kisumutalent <homiemusa@gmail.com>
2018-06-28 08:00:51 +0300

    Creating signup

---
+++ system.py
@@ -0,0 +1,16 @@
+class User():
+
+    #enables users to create accounts
+    def register(self):
+        print"*****************************"
+        print"Welcome To Our System"
+        print " "
+        full_name = raw_input("Enter Your Names")
+        username  = raw_input("Enter Your Username")
+        email     = raw_input("Enter Your Email")
+        password  = raw_input("Enter Your password")
+        confirm_password = raw_input("Confirm Your passward")
+
+        print"You are successfully Signup"
+        print " " 
+        
