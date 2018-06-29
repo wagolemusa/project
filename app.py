6774d330e1d1d9423273e1c56fb2c040c1593ca1
kisumutalent <homiemusa@gmail.com>
2018-06-29 17:32:59 +0300

    setting up routes

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify, render_template, session, request, url_for, flash
 
+from functools import wraps
 
 app = Flask(__name__)
+
+app.secrect_key = "my precious"
 
 
 @app.route('/')
 def home():
 	return render_template('index.html')
+
+
 
 
 if __name__ == '__main__':
