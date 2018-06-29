0adbb51d288a11672ed6c8f2e6cb96f022df15f0
kisumutalent <homiemusa@gmail.com>
2018-06-29 18:52:46 +0300

    updating routes

--- app.py
+++ app.py
@@ -11,8 +11,18 @@ app.secrect_key = "my precious"
 def home():
 	return render_template('index.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+	return render_template('post.html')
+	
 
 if __name__ == '__main__':
 	app.run(debug=True)
