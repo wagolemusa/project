b03f3d775d6078422e858212c6bd139f5d913cd8
kisumutalent <homiemusa@gmail.com>
2018-06-30 07:50:56 +0300

    updating login creadentials

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, jsonify, render_template, session, request, url_for, flash
+from flask import Flask, jsonify, render_template, session, request, url_for, flash, redirect
+from functools import wraps
+from wtforms import Form
 
 from register import Registers
 from comment import Post_comment
 
 app = Flask(__name__)
 
-app.secrect_key = "my precious"
+app.secret_key = "my precious"
 
 Registers = Registers()
 Post_comment = Post_comment()
@@ -34,17 +36,18 @@ def register():
 def login():
 	error = None
 	if request.method == 'POST':
-		if request.form['username'] != 'refuge' or request.form['password'] != 'wise':
-			error  = 'Invalid credentials please try agian.'
+		if request.form['username'] != 'refuge' or request.form['password'] != 'wise@1':
+			error = 'Invalid credentials. Please try agian.'
 		else:
 			session['logged_in'] = True
-			flask('You are just logged in')
+			flash('You were just logged in!')
 			return redirect(url_for('post'))
-			return render_template('login.html', error=error)
+	return render_template('login.html', error=error)
     
 @app.route('/user_details')
 def view_register():
 	return render_template('user_details.html', registers = Registers)
+
 
 
 @app.route('/post', methods=['GET', 'POST'])
@@ -53,4 +56,4 @@ def post():
 	
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run()
--- comment.py
+++ comment.py
@@ -2,7 +2,7 @@ def Post_comment():
 	comment = [
 			{
 			'user_id': 1,
-			'post_id': 1
+			'post_id': 1,
 			'comment' : 'That is not True'
 		}
 
