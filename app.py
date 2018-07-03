365649a7e56d559e10ce7cc50f2e367ec2320abb
kisumutalent <homiemusa@gmail.com>
2018-07-03 14:00:45 +0300

    modifing all routes

--- app.py
+++ app.py
@@ -18,9 +18,34 @@ app.secret_key = "refuge wise"
 Registers = Registers()
 Post_comments = Post_comments()
 
-data = []
-register_info = {}
-comments = {}
+registers = [
+		{
+				'user_id': 1,
+    		'full_name':'refuge wise',
+    		'username': 'refuge',
+    		'email':  'wise@gmail.com',
+    		'password': 'wise@1',
+    		'confirm_password':'123456'
+
+		},
+		{
+				'user_id':2,
+    		'full_name':'opio wafula',
+    		'username' : 'opio',
+    		'email': 'wafula@gmail.com',
+    		'password':'me1223',
+    		'confirm_password':'me1223'
+		}
+]
+
+comments = {
+				
+			'user_id': 1,
+			'post_id': 1,
+			'post' : 'That is not True'	
+}
+
+
 
 
 # Validates logged in user with session
@@ -39,15 +64,17 @@ def get_password(username):
         return 'python'
     return None
 
+#home route
 @app.route('/', methods=['GET'])
 @auth.login_required
 def index():
 	return jsonify({'message': 'Welcome to our system'})
 
+#User Register 
 @app.route('/api/auth/register', methods=['GET', 'POST'])
 def register():
 	user = {
-		'user_id': registers[-1]['user_id'] + 1,
+		#'user_id': registers[-1]['user_id'] + 1,
 		'full_name': request.json['full_name'],
 		'username' :request.json['username'],
 		'email' : request.json['email'],
@@ -57,9 +84,27 @@ def register():
 	registers.append(user)
 	return jsonify({'user': user}),201
 
+# Post comment
+@app.route('/api/v1/post-comment', methods=['GET', 'POST'])
+def post_comment():
+	comm = {
+		'post_id': comm[-1]['post_id'] + 1,
+		'user_id': request.json.get['user_id'],
+		'comment' : request.json['comment']
+	}
+	comments.append(comm)
+	return jsonify({'comm': comm })
 
+@app.route('/api/v1/view_comment', methods=['GET'])
+def veiw_comment():
+	return jsonify({'comments': comments })
 
-	
+# User Account
+@app.route('/api/v1/account', methods=['GET'])
+def user_account():
+	return jsonify({'registers': registers })
+
+#User Logout
 @app.route('/logout')
 #@login_required
 def logout():
